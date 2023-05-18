57dbe863e0263b5d41cc59b252188efe7d8187d6
knapp <emiliodamasco04@gmail.com>
2023-05-18 05:51:12 -0700

    added selection sort function prototype

--- sort.h
+++ sort.h
@@ -23,6 +23,7 @@ void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 void swap(int *n1, int *n2);
 listint_t *insert(listint_t *sorted, listint_t *node);
