6ecc53557a946a8a8329699d8306d02b593038c8
knapp <emiliodamasco04@gmail.com>
2023-05-18 05:49:45 -0700

    added selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,24 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j, min, temp;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        min = i;
+
+        for (j = i + 1; j < size; j++)
+            if (array[min] > array[j])
+                min = j;
+
+        if (min != i)
+        {
+            temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+
+        print_array(array, size);
+    }
+}
